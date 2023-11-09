0a3ec3ea176e913653e3dab849de14cfbdffe8ff
Michael Whitt <michaelwhitt90@gmail.com>
2023-11-09 12:55:54 -0600

    fix(server): add routing

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 3000
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Parse JSON requests
-app.use(express.json());
+app.use(express.json())
 
 // Handle DELETE request at '/delete' endpoint
 app.delete('/delete', (req, res) => {
@@ -26,14 +26,30 @@ app.get("/slides", (req, res) => {
   console.log(num, v)
   if (num <= 1 && v === -1) {
     num = 0
-    res.sendFile(__dirname+`/public/slides/intro.html`);
+    res.sendFile(__dirname+`/public/slides/intro.html`)
   } else {
     console.log('here')
     num += v
-    res.sendFile(__dirname+`/public/slides/${num}.html`);
+    res.sendFile(__dirname+`/public/slides/${num}.html`)
   } 
- });
+ })
+
+const contentStack = ['intro.html', '1.html', '2.html', '3.html', '4.html', '5.html', '6.html', '7.html', '8.html', '9.html', '10.html']
+let currentIndex = 0
+
+app.get('/get_content', (req, res) => {
+  const direction = req.query.direction
+  
+  if (direction === 'previous') {
+    currentIndex = Math.max(0, currentIndex - 1)
+  } else if (direction === 'next') {
+    currentIndex = Math.min(contentStack.length - 1, currentIndex + 1)
+  }
+  res.sendFile(__dirname+`/public/slides/${currentIndex ? currentIndex : 'intro'}.html`)
+})
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
